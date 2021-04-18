c8fb9268c6ac45bfba7cac9b301202c6813310cd
jarekj9 <jerry.esp.sha@gmail.com>
2021-04-18 09:26:55 +0200

    correction

---
+++ js/data.js
@@ -0,0 +1,3 @@
+OPEN_WEATHERMAP_KEY = "your key";
+WEATHER_URL = "https://api.openweathermap.org/data/2.5/onecall?lat=<LAT>&lon=<LON>&units=metric&exclude=minutely&daily&appid=" + OPEN_WEATHERMAP_KEY;
+REV_GEOLOCATION_URL = "http://api.openweathermap.org/geo/1.0/reverse?lat=<LAT>&lon=<LON>&limit=1&appid=" + OPEN_WEATHERMAP_KEY;
